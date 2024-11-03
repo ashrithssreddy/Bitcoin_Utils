e3610146d6248a169274da0866af2923475df0eb
Ashrith Reddy <5827632+ashrithssreddy@users.noreply.github.com>
2024-11-03 16:28:06 -0600

    Added binary seed

--- utils/generate_all_keys.py
+++ utils/generate_all_keys.py
@@ -6,3 +6,13 @@ import base58
 # Seed phrase
 seed_phrase = "oxygen business lecture cream sad write vote fly rate also ozone type"
 print("Seed:", seed_phrase, "\n")
+
+#### Binary Seed
+# Initialize the Mnemonic object
+mnemo = Mnemonic("english")
+print("Mnemonic Object:", mnemo, "\n")  # Printing the mnemonic object
+
+# Generate the binary seed from the seed phrase
+binary_seed = mnemo.to_seed(seed_phrase)
+print("Binary Seed:", binary_seed, "\n") # hexadecimal format for readability
+print("Binary Seed (hex):", binary_seed.hex(), "\n") # hexadecimal format for readability
