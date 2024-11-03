8cc1d909d8f82d65b4cff0f91540e4510129583c
Ashrith Reddy <5827632+ashrithssreddy@users.noreply.github.com>
2024-11-03 16:27:37 -0600

    Generate all keys - boilerplate

---
+++ utils/generate_all_keys.py
@@ -0,0 +1,8 @@
+from mnemonic import Mnemonic
+from bip32utils import BIP32Key
+import hashlib
+import base58
+
+# Seed phrase
+seed_phrase = "oxygen business lecture cream sad write vote fly rate also ozone type"
+print("Seed:", seed_phrase, "\n")
